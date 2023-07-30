387eaaf620a967d074bb4cf40caed9fd4832cba7
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-07-30 19:25:21 +0300

    move mimiron out to snother project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use anyhow::Result;
+
 use clap::{Parser, Subcommand};
 
-mod mimiron;
 mod salary;
 mod vat;
 
@@ -20,17 +19,15 @@ enum Commands {
     /// Get salary devisions from a final salary
     Salary(salary::SalaryArgs),
 
-    /// Make Hearthstone API enquiries. Work in progress.
-    Mimiron(mimiron::MimironArgs),
+
 }
 
-pub fn run() -> Result<()> {
+pub fn run()  {
     let cli = Cli::parse();
     match cli.command {
         Commands::Vat(args) => vat::run(args),
         Commands::Salary(args) => salary::run(args),
-        Commands::Mimiron(args) => mimiron::run(args)?,
     };
 
-    Ok(())
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
-fn main() -> anyhow::Result<()> {
+fn main()  {
     hsb::run()
 }
